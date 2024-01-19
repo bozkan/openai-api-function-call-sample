2cb50e3ac6c629f8d4b6c600f0e209dde6ce1dc4
Bugra Ozkan <bugraozkan@live.com>
2024-01-19 18:03:30 +0000

    Decrease maxDuration

--- app/chat/function/route.js
+++ app/chat/function/route.js
@@ -10,7 +10,7 @@ import reserve_hotel from '../../../lib/reserve_hotel.json'
 import get_reservation from '../../../lib/get_reservation.json'
 
 export const dynamic = 'force-dynamic'
-export const maxDuration = 60
+export const maxDuration = 10
 
 export async function POST(request) {
 
